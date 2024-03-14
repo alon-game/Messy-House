1aa3273f586390981febcc18790a568e2b2b0a7f
aloneshed <aloneshed1@gmail.com>
2024-03-14 19:58:16 +0200

    update files

--- Assets/House/Scripts/Timer.cs
+++ Assets/House/Scripts/Timer.cs
@@ -7,6 +7,10 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float remainingTime;
+    [SerializeField] float warningTime = 30f;
+    private Color warningColor = Color.red;
+
+    private bool isWarningDisplayed = false;
 
     // Update is called once per frame
     void Update()
@@ -14,14 +18,30 @@ public class Timer : MonoBehaviour
         if (remainingTime > 0)
         {
             remainingTime -= Time.deltaTime;
+            UpdateTimerText();
+            CheckWarning();
         }
-        else if (remainingTime < 0)
+        else
         {
             remainingTime = 0;
+            timerText.text = "00:00";
         }
+    }
+
+    void UpdateTimerText()
+    {
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void CheckWarning()
+    {
+        if (remainingTime <= warningTime && !isWarningDisplayed)
+        {
+            timerText.color = warningColor;
+            isWarningDisplayed = true;
+        }
     }
 
     public void AddTime(float timeToAdd)
